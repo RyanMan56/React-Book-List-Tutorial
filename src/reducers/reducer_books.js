1a4b928e0479727e13becb7c2c2c34a8014d849e
Ryan Jones <Ryan Jones>
2017-03-03 17:52:14 +0000

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript The Good Parts' },
+    { title: 'Harry Potter' },
+    { title: 'Children of Time' },
+    { title: 'The Dark Tower' },
+  ]
+}
