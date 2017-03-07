952d1c1938e83c134546ab575267a540bbf71593
Ryan Jones <Ryan Jones>
2017-03-07 22:53:24 +0000

    Redux stuff

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
-export default function() {
+export default function () {
   return [
-    { title: 'Javascript The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'Children of Time' },
-    { title: 'The Dark Tower' },
-  ]
+    { title: 'Javascript The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 39 },
+    { title: 'Children of Time', pages: 235 },
+    { title: 'The Dark Tower', pages: 85 },
+  ];
 }
